192874f3b2a8c0c19b61007023292a091fa91aae
Lucas Santos <lucas.silva1203@outlook.com.br>
2025-02-15 19:27:54 -0300

    feat: add dto validation

--- src/api/app.module.ts
+++ src/api/app.module.ts
@@ -4,14 +4,18 @@ import { ForumModule } from './forum/forum.module';
 import { PostModule } from './post/post.module';
 import { SettingsModule } from './settings/settings.module';
 import { UserModule } from './user/user.module';
+import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
+    AuthModule,
     CommentaryModule,
     ForumModule,
     PostModule,
     SettingsModule,
     UserModule,
+    ConfigModule.forRoot({ isGlobal: true }),
   ],
 })
 export class AppModule {}
