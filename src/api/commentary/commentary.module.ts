de5cb77a5d9bbf9411c7390ec411196dcd05a9fc
Lucas Santos <lucas.silva1203@outlook.com>
2025-10-08 18:06:17 -0300

    fix: add missing provider

--- src/api/commentary/commentary.module.ts
+++ src/api/commentary/commentary.module.ts
@@ -3,6 +3,8 @@ import { CommentaryController } from './commentary.controller';
 import { CommentaryService } from './commentary.service';
 import { CommentaryRepository } from './repository/commentary.repository';
 import { COMMENTARY_REPOSITORY_TOKEN } from './repository/commentary.repository.base';
+import { USER_REPOSITORY_TOKEN } from '../user/repository/user.repository.token';
+import { UserRepository } from '../user/repository/user.repository';
 
 @Module({
   imports: [],
@@ -10,6 +12,7 @@ import { COMMENTARY_REPOSITORY_TOKEN } from './repository/commentary.repository.
   providers: [
     CommentaryService,
     { provide: COMMENTARY_REPOSITORY_TOKEN, useClass: CommentaryRepository },
+    { provide: USER_REPOSITORY_TOKEN, useClass: UserRepository },
   ],
   exports: [COMMENTARY_REPOSITORY_TOKEN],
 })
