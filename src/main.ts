b03c6f613025e61d8d0f47e4d4cde294a37f0e72
Lucas Santos <lucas.silva1203@outlook.com>
2026-04-04 19:41:45 -0300

    refactor: replace ProtectRoutes with GatekeeperGuard in main bootstrap

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,13 @@ import { AppModule } from './api/app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { ProtectRoutes } from './utils/decorators/protect-routes/PublicDecorator';
 import { HttpExceptionFilter } from './exceptions/filter';
+import { GatekeeperGuard } from './utils/guards/GatekeeperGuard';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  app.useGlobalGuards(new ProtectRoutes(new Reflector()));
+  //app.useGlobalGuards(new ProtectRoutes(new Reflector()), );
+  app.useGlobalGuards(new GatekeeperGuard(new Reflector()), );
   app.useGlobalFilters(new HttpExceptionFilter());
 
   app.enableCors({
