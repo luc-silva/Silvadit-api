284a9b373f7ba8363245ea97a1550ad07673d640
Lucas Santos <lucas.silva1203@outlook.com>
2026-04-04 19:41:21 -0300

    refactor: enhance user extraction logic and token parsing

--- src/utils/decorators/extract-user/index.ts
+++ src/utils/decorators/extract-user/index.ts
@@ -4,6 +4,7 @@ import {
   UnauthorizedException,
 } from '@nestjs/common';
 import { Request } from 'express';
+import { UserRepository } from '~/api/user/repository/user.repository';
 import { JWTEncrypter } from '~/utils/Encrypter';
 
 export const ExtractSession = createParamDecorator(
@@ -17,17 +18,35 @@ export const ExtractSession = createParamDecorator(
   },
 );
 
-export const ExtractUser = createParamDecorator(
-  (_: unknown, ctx: ExecutionContext) => {
-    const request = ctx.switchToHttp().getRequest<Request>();
-    return parseToken(request);
-  },
-);
-
 export const parseToken = (request: Request) => {
   try {
     const token = request.user!.replace('Bearer', '');
     return JWTEncrypter.decode<ISession>(token.trim());
+  } catch (error) {
+    throw new UnauthorizedException('Invalid Token');
+  }
+};
+
+export const ExtractUser = createParamDecorator(
+  async (_: unknown, ctx: ExecutionContext) => {
+    const request = ctx.switchToHttp().getRequest<Request>();
+    const session = parseToken(request);
+
+    const userRepository = new UserRepository();
+    const userFound = await userRepository.getUserByIdOrUsername(session.id);
+
+    if (!userFound) {
+      throw new Error('User not found.');
+    }
+
+    return { user: userFound, session };
+  },
+);
+
+export const parseSessionToken = (token: string) => {
+  try {
+    const session = token.replace('Bearer', '');
+    return JWTEncrypter.decode<ISession>(session.trim());
   } catch (error) {
     throw new UnauthorizedException('Invalid Token');
   }
